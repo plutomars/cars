9be05ab4b2cec4af49947ee41bca302d7b6fcadf
Derek Lu <dereklu0529@gmail.com>
2018-09-30 15:45:51 -0700

    add car search data source add MainActivity

--- app/src/main/java/com/example/pluto/cars/MainActivity.java
+++ app/src/main/java/com/example/pluto/cars/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.pluto.cars;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mBuyButton;
+    private Button mSellButton;
+    private TextView mWelcomeTextView;
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG,"OnCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBuyButton = (Button) findViewById(R.id.btnBuy);
+        mBuyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mSellButton = (Button)findViewById(R.id.btnSell);
+        mSellButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mWelcomeTextView = (TextView)findViewById(R.id.txtViewWelcome);
+
+
     }
 }
