0fdd2b90f2dcab99d408d749a8cec1b2076e120a
Maakaron <bonebess@gmail.com>
2018-12-10 15:07:04 -0800

    main_activity swipe

--- app/src/main/java/com/example/pluto/cars/MainActivity.java
+++ app/src/main/java/com/example/pluto/cars/MainActivity.java
@@ -4,13 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int MIN_DISTANCE = 100;
+    private static final int MAX_OFF_PATH = 200;
+    private static final int THRESHOLD_VELOCITY = 100;
+    private GestureDetector mGestureDetector;
     private Button mBuyButton;
     private Button mSellButton;
     private TextView mWelcomeTextView;
@@ -21,7 +28,17 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG,"OnCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGestureDetector = new GestureDetector(this, new SwipeDetector());
 
+// Set touch listener to parent view of activity layout
+// Make sure that setContentView is called before setting touch listener.
+        findViewById(R.id.linear).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // Let gesture detector handle the event
+                return mGestureDetector.onTouchEvent(event);
+            }
+        });
         mBuyButton = (Button) findViewById(R.id.btnBuy);
         mBuyButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,7 +61,32 @@ public class MainActivity extends AppCompatActivity {
 
         mWelcomeTextView = (TextView)findViewById(R.id.txtViewWelcome);
 
-    }
 
+    }
+    private class SwipeDetector extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            if (e1 != null && e2 != null) {
+                float dy = e1.getY() - e2.getY();
+                float dx = e1.getX() - e2.getX();
+
+                // Right to Left swipe
+                if (dx > MIN_DISTANCE && Math.abs(dy) < MAX_OFF_PATH &&
+                        Math.abs(velocityX) > THRESHOLD_VELOCITY) {
+                    startActivity(new Intent(MainActivity.this, SellActivity.class));
+                    return true;
+                }
+
+                // Left to right swipe
+                else if (-dx > MIN_DISTANCE && Math.abs(dy) < MAX_OFF_PATH &&
+                        Math.abs(velocityX) > THRESHOLD_VELOCITY) {
+                    // Below is sample code to show left to right swipe while launching next activity
+                    startActivity(new Intent(MainActivity.this,SearchActivity.class));
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 
 }
